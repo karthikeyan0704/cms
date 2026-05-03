826dd6915929b568e6e49789eb1d54ebdf781ae1
karthikeyan0704 <karthikeyangnanasekar07@gmail.com>
2026-05-03 22:11:57 +0530

    Fixed Vercel config and DB crash

--- config/db.js
+++ config/db.js
@@ -6,9 +6,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected...');
   } catch (err) {
-    console.error(err.message);
-    // Exit process with failure
-    process.exit(1);
+    console.error('MongoDB Connection Error:', err.message);
   }
 };
 
